c22d13d80dd137ca97426b4cf846741b397600b0
Jatin More <54055686+jatinmore@users.noreply.github.com>
2021-08-24 00:12:11 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var buyPrice = document.querySelector('#buy-price');
+var buyQty = document.querySelector('#qty');
+var ltp = document.querySelector('#ltp');
+var outputDisplay = document.querySelector('#show-output');
+var btnCheck=document.querySelector('#btn-submit');
+
+btnCheck.addEventListener('click',calculateValue);
+
+function calculateValue()
+{
+    var bp =Number(buyPrice.value);
+    var qty =Number(buyQty.value);
+    var mp =Number(ltp.value);
+    const investmentValue = bp*qty;
+    const currentMarketValue = qty*mp;
+    const profitAndLoss = currentMarketValue - investmentValue ;
+    const percentageVal = profitAndLoss / bp * 100 ;
+    outputDisplay.innerText='Your Investment Profit / Loss is =' + profitAndLoss + ' In Percentage = ' + percentageVal +'%'
+    return profitAndLoss
+}
